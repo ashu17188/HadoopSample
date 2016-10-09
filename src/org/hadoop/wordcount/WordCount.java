413012fc97f7ae9a872b883834268a2ac1c665d5
ashu17188 <ashu17188@gmail.com>
2016-10-09 17:21:35 +0530

    Input file File.txt added

--- src/org/hadoop/wordcount/WordCount.java
+++ src/org/hadoop/wordcount/WordCount.java
@@ -46,34 +46,4 @@ public class WordCount extends Configured implements Tool {
 	    
 	    return job.waitForCompletion(true) ? 0 : 1;
 	  }
-
-/*	  public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
-	    private final static IntWritable one = new IntWritable(1);
-	    private Text word = new Text();
-
-	    @Override
-	    public void map(LongWritable key, Text value,
-	                    Mapper.Context context) throws IOException, InterruptedException {
-	      String line = value.toString();
-	      StringTokenizer tokenizer = new StringTokenizer(line);
-	      while (tokenizer.hasMoreTokens()) {
-	        word.set(tokenizer.nextToken());
-	        context.write(word, one);
-	      }
-	    }
-	  }*/
-
-	 /* public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
-
-	    @Override
-	    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-	      int sum = 0;
-	      for (IntWritable value : values) {
-	        sum += value.get();
-	      }
-
-	      context.write(key, new IntWritable(sum));
-	    }
-	  }
-*/
 	}
